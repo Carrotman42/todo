81c8762dfa84404fc7267408e1394494cf056152
Kevin Malachowski <kevin@chowski.com>
2014-02-16 21:38:09 -0500

    Initial server code!

---
+++ todo.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"io"
+)
+
+func write(w io.Writer, s string) {
+	io.WriteString(w, s)
+}
+
+func main() {
+	fmt.Println("This is your to-do list")
+	
+	http.HandleFunc("/", MainPage)
+	http.ListenAndServe(":16005", nil)
+}
+
+func MainPage(w http.ResponseWriter, r *http.Request) {
+	write(w, "Hello!")
+}
